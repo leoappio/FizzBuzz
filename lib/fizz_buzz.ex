584209d4eec46c949750e1eef9e055e9cc847f2c
Leonardo Appio <leonardo@okai.com.br>
2025-02-28 11:18:10 -0300

    refactor to use pipe operator

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,9 +1,10 @@
 defmodule FizzBuzz do
 
   def build(file_name) do
-    file = File.read(file_name)
-    handle_file_read(file)
-    end
+    file_name
+    |> File.read()
+    |>handle_file_read()
+  end
 
   def handle_file_read({:ok, result}), do: result
   def handle_file_read({:error, reason}), do: reason
