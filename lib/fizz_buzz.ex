65218beacc11da4ac0961838fee1176f43a0fd26
Leonardo Appio <leonardo@okai.com.br>
2025-02-28 11:15:50 -0300

    refactor using pattern matching

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,9 +1,11 @@
 defmodule FizzBuzz do
 
-  def build(file_name)do
-    case File.read(file_name) do
-      {:ok, result} -> result
-      {:error, reason} -> reason
+  def build(file_name) do
+    file = File.read(file_name)
+    handle_file_read(file)
     end
+
+  def handle_file_read({:ok, result}), do: result
+  def handle_file_read({:error, reason}), do: reason
+
   end
-end
